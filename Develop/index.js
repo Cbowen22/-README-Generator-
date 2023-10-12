2a4ff8bf57a0a0ab2df6ec1db31283c0fda0bc3a
Corinne Bowen <132027542+Cbowen22@users.noreply.github.com>
2023-10-11 20:34:31 -0400

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,72 +1,61 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        message: 'What is your Github Username?',
-        name: 'Github Username',
-    },
-    {
-        type: 'input',
-        message: 'What is your Email Address?',
-        name: 'Email Address',
-    },
-    {
-        type: 'input',
         name: 'title',
-        message: "What is your project's name?",
-      },
-      {
+        message: 'What is the name of your project?',
+    },
+     {
         type: 'input',
         name: 'description',
-        message: 'Please write a short description of your project',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'What kind of license should your project have?',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-      },
-      {
+        message: 'Provide a description of you project.',
+    },
+    {
         type: 'input',
         name: 'installation',
         message: 'What command should be run to install dependencies?',
         default: 'npm i',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'test',
-        message: 'What command should be run to run tests?',
+        message: 'What command should be used to run tests?',
         default: 'npm test',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'usage',
         message: 'What does the user need to know about using the repo?',
-      },
-      {
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license is assigned to this project?',
+        choices: ['None', 'Apache 2.0 License', 'MIT License', 'Mozilla Public License 2.0', 'The Unlicense'],
+    },
+    {
         type: 'input',
         name: 'contributing',
-        message: 'What does the user need to know about contributing to the repo?',
-      },
+        message: 'What does the user need to know about contributing to the project?',
+    },
+    {
+        type: 'input',
+        name: 'userName',
+        message: 'What is your github user name?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },  
 ];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.createPromptModule(questions).then((inquirerResponses) => {
-        console.log('Generating');
-        writeToFile('README.md', generateMarkdown({...inquirerResponses}));
-    });
-}
-
-// Function call to initialize app
-init();
+inquirer
+    .prompt(questions)
+    .then((answers) => {
+        const readMeContent = generateMarkdown(answers);
+        fs.writeFile('./Output/README.md', readMeContent, (err) =>
+          err ? console.log(err) : console.log('Successfully created README.md! Check the Output folder!')
+        );
+      });
